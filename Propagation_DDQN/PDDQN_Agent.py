9fda74c50154f6c96de1680d17ee0d9550bb5874
Howard <howard@HowarddeMacBook-Pro.local>
2018-03-24 11:59:03 +0800

    add pddqn

---
+++ Propagation_DDQN/PDDQN_Agent.py
@@ -0,0 +1,159 @@
+# -*- coding: UTF-8 -*-
+"""
+by Howard
+using:
+- python: 3.6
+- Tensorflow: 1.2.1
+- gym: 0.9.2
+"""
+
+import numpy as np
+import random
+import sys
+# sys.path.append("..")
+from DQN.Agent import Agent as agent
+import operator
+import math
+
+
+class Agent(agent):
+    def __init__(
+        self,
+        brain,  # 使用的神经网络
+        observation_space_shape,
+        n_actions,  # 动作数
+        reward_decay=0.9,  # gamma参数
+        MAX_EPSILON=0.9,  # epsilon 的最大值
+        MIN_EPSILON=0.01,  # epsilon 的最小值
+        LAMBDA=0.001,  # speed of decay
+        memory_size=500,  # 记忆的大小
+        batch_size=32,  # 每次更新时从 memory 里面取多少记忆出来
+        replace_target_iter=300,  # 更换 target_net 的步数
+    ):
+        super(Agent, self).__init__(brain, observation_space_shape, n_actions,  reward_decay, MAX_EPSILON,
+                                    MIN_EPSILON,  LAMBDA, memory_size, batch_size, replace_target_iter)
+        self.horizon_K = 4
+        self.print_q = 0
+
+    def calculating_q_lower_max(self, batch_memory_index):
+        q_lower_max = []
+        for index in batch_memory_index:
+            q_lower = []
+            for k in range(1, min(self.horizon_K, len(self.memory) - index - 1) + 1):
+                cumulative_r = 0
+                for i in range(0, k + 1):
+                    next_index = index + i
+                    cumulative_r += math.pow(self.gamma, i) * self.memory[next_index][2]
+                if self.memory[index + k][4]:
+                    q_next = 0
+                    q_lower.append(cumulative_r + math.pow(self.gamma, k + 1) * np.max(q_next))
+                else:
+                    q_next = self.brain.predict_target_action([self.memory[index + k][3]])
+                    q_eval = self.brain.predict_eval_action([self.memory[index + k][3]])
+                    # if operator.eq(self.memory[index + k + 1][0], self.memory[index + k][3]) is not True:
+                    #     print('----------------------------------------------------------')
+                    #     print(self.memory[index + k + 1][0], self.memory[index + k][3])
+                    # print('q_next', q_next)
+                    max_index = np.argmax(q_eval)
+                    q_lower.append(cumulative_r + math.pow(self.gamma, k + 1) * q_next[0][max_index])
+            if q_lower:
+                q_lower_max.append(max(q_lower))
+            else:
+                q_lower_max.append(0)
+        return np.array(q_lower_max)
+
+    def calculating_q_upper_min(self, batch_memory_index):
+        q_upper_min = []
+        for index in batch_memory_index:
+            q_upper = []
+            for k in range(1, min(self.horizon_K, index - 1) + 1):
+                cumulative_r = 0
+                for i in range(0, k + 1):
+                    next_index = index - k - 1 + i
+                    cumulative_r += math.pow(self.gamma, i - k - 1) * self.memory[next_index][2]
+
+                memory_before = self.memory[index - k - 1]
+                q_before = self.brain.predict_target_action([memory_before[0]])
+                q_eval = self.brain.predict_eval_action([memory_before[0]])
+                max_index = np.argmax(q_eval)
+                # print('q_before', q_before[0])
+                # print('q', (math.pow(self.gamma, -k - 1) * q_before[0][self.memory[index - k - 1][1]]))
+                # print('cumulative_r', cumulative_r)
+
+                # q_upper.append((math.pow(self.gamma, -k - 1) * q_before[0][memory_before[1]]) - cumulative_r)
+                q_upper.append(math.pow(self.gamma, -k - 1) * q_before[0][max_index] - cumulative_r)
+            if q_upper:
+                q_upper_min.append(min(q_upper))
+            else:
+                q_upper_min.append(0)
+
+        return np.array(q_upper_min)
+
+    def learn(self, costFlag=False):
+        # 每隔 replace_target_iter 步 替换 target_net 参数
+        if self.learn_step_counter % self.replace_target_iter == 0:
+            self.brain.replace_target_params()
+            print('\nlearn_step_counter=', self.learn_step_counter, ' target_params_replaced')
+            print('epsilon:', self.epsilon, '\n')
+            print('printq', self.print_q)
+
+        # 从 memory 中随机抽取 batch_size 大小的记忆
+        batch_size = min(self.batch_size, len(self.memory))
+        # batch_memory = random.sample(self.memory, batch_size)
+        batch_memory_index = random.sample(range(len(self.memory)), batch_size)
+        batch_memory = [self.memory[index] for index in batch_memory_index]
+        # print('\nbatch_memory_index', batch_memory_index)
+        # no_state = np.zeros(self.observation_space_shape)
+        states = np.array([o[0] for o in batch_memory])
+        states_ = np.array([o[3] for o in batch_memory])
+        action = np.array([o[1] for o in batch_memory])
+        reward = np.array([o[2] for o in batch_memory])
+
+        q_next = self.brain.predict_target_action(states_)
+        '''
+        q_target_ = []
+        for i in range(0, batch_size):
+            q_target_.append(reward[i] + self.gamma * np.max(q_next[i]))
+        下面的损失了一些精度
+        '''
+        # q_target = reward + self.gamma * np.max(q_next, axis=1)
+
+        # ---------------------- difference with DDQN ---------------------------
+        q_eval = self.brain.predict_eval_action(states_)
+        q_target = []
+        for i in range(0, batch_size):
+            done = batch_memory[i][4]
+            if done:
+                q_target.append(reward[i])
+            else:
+                max_index = np.argmax(q_eval[i])
+                q_target.append(reward[i] + self.gamma * q_next[i][max_index])
+        q_lower_max = self.calculating_q_lower_max(batch_memory_index)
+        q_upper_min = self.calculating_q_upper_min(batch_memory_index)
+
+        for i in range(0, batch_size):
+            if q_lower_max[i] < q_upper_min[i]:
+                if q_target[i] < q_lower_max[i]:
+                    q_target[i] = q_lower_max[i]
+                elif q_target[i] > q_upper_min[i]:
+                    q_target[i] = q_upper_min[i]
+
+        self.print_q = q_target
+        # ----------------------------------------------------------------------
+        # One Hot Encoding
+        one_hot_action = np.eye(self.n_actions)[action]
+        # 训练 eval 神经网络
+        if costFlag:
+            cost = self.brain.train(states, q_target, one_hot_action, True)
+            self.cost_his.append(cost)
+        else:
+            self.brain.train(states, q_target, one_hot_action)
+        # brain 中 的 output_graph 需要为 True
+        self.brain.output_tensorboard(states, q_target, states_, one_hot_action, self.learn_step_counter)
+        self.learn_step_counter += 1
+
+
+if __name__ == '__main__':
+    Brain = brain(n_actions=1, n_features=1, output_graph=True)
+    agent = Agent(Brain, n_actions=1, n_features=1,)
+    agent.plot_cost()
---
+++ Propagation_DDQN/run_exp.py
@@ -0,0 +1,88 @@
+# -*- coding: UTF-8 -*-
+import gym
+from MLP_Brain import MLP_Brain as Brain
+from PDDQN_Agent import Agent
+import numpy as np
+from utls import data_save
+from pqExp_config import configs
+
+env_name = 'CartPole-v0'
+env = gym.make(env_name)   # 定义使用 gym 库中的那一个环境
+env = env.unwrapped  # 注释掉的话 每局游戏 reward之和最高200
+env.seed(1)
+print(env.action_space.sample())  # 查看这个环境中可用的 action 有多少个
+print(env.observation_space.shape)    # 查看这个环境中可用的 state 的 observation 有多少个
+print(env.observation_space.high)   # 查看 observation 最高取值
+print(env.observation_space.low)    # 查看 observation 最低取值
+
+print('\nThe config:\n', configs, '\n')
+neurons_per_layer = configs['Brain']['neurons_per_layer']
+learning_rate = configs['Brain']['learning_rate']
+output_graph = configs['Brain']['output_graph']
+restore = configs['Brain']['restore']
+
+reward_decay = configs['Agent']['reward_decay']
+replace_target_iter = configs['Agent']['replace_target_iter']
+memory_size = configs['Agent']['memory_size']
+batch_size = configs['Agent']['batch_size']
+MAX_EPSILON = configs['Agent']['MAX_EPSILON']
+MIN_EPSILON = configs['Agent']['MIN_EPSILON']
+LAMBDA = configs['Agent']['LAMBDA']
+
+# learning_rate 重要
+# restore 和 MAX_EPSILON 一起调整
+brain = Brain(
+    n_actions=env.action_space.n,
+    n_features=env.observation_space.shape[0],
+    neurons_per_layer=neurons_per_layer,
+    learning_rate=learning_rate,
+    output_graph=output_graph,
+    restore=restore,
+)
+agent = Agent(
+    brain=brain,
+    n_actions=env.action_space.n,
+    observation_space_shape=env.observation_space.shape,
+    reward_decay=reward_decay,
+    replace_target_iter=replace_target_iter,
+    memory_size=memory_size,
+    batch_size=batch_size,
+    MAX_EPSILON=MAX_EPSILON,
+    MIN_EPSILON=MIN_EPSILON,
+    LAMBDA=LAMBDA,
+)
+
+
+def run_game(episode, env, Agent, plt_q=False):
+    # set_memory_with_random()
+    total_steps = 0
+    # q_change = [[0.03073904, 0.00145001, -0.03088818, -0.03131252]]
+    for i_episode in range(episode):
+        observation = env.reset()
+        if plt_q:
+            q_change = [observation]
+            action_change = Agent.choose_action(observation)
+        totalR = 0
+        while True:
+            # env.render()
+            action = Agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            totalR += reward
+            Agent.store_memory(observation, action, reward, observation_, done)
+            Agent.learn(costFlag=done)
+            observation = observation_
+            total_steps += 1
+            if done:
+                print('episode: ', i_episode, ' epsilon: ', Agent.epsilon, 'total_reward:', totalR)
+                if plt_q:
+                    Agent.statistical_values(totalR, q_change, action_change)
+                else:
+                    Agent.statistical_values(totalR)
+                break
+
+    # Brain.save()  # 存储神经网络
+    # Agent.plot_values()
+
+
+run_game(600, env, agent, True)
+data_save("PDDQN", env_name, brain, agent)
